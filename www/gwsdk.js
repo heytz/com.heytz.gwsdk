2c2cd01c1d0c6af3a2f148a928c3e87ec047579c
陈东东 <kevinchen@infapp.com>
2016-02-29 13:12:29 +0800

    添加接口 js的引用

--- www/gwsdk.js
+++ www/gwsdk.js
@@ -7,7 +7,12 @@ var productToArray = function (products) {
         proArr.push(products);
     return proArr;
 }
-exports.setDeviceWifi = function (appid, productKey, wifiSSID, wifiKey,timeout, success, error) {
+/**
+ *  cordova 配对设备上网
+ *
+ *  @param command [appid,"",ssid,pwd,timeout]
+ */
+exports.setDeviceWifi = function (appid, productKey, wifiSSID, wifiKey, timeout, success, error) {
     exec(success, error, "gwsdk", "setDeviceWifi", [appid, productToArray(productKey), wifiSSID, wifiKey, timeout ? timeout : 60,]);
 };
 /**
@@ -22,9 +27,9 @@ exports.setDeviceWifi = function (appid, productKey, wifiSSID, wifiKey,timeout, 
  * @param timeout   默认: 60s
  * @param mode      默认: AirLink
  * @param softAPSSIDPrefix
-          SoftAPMode模式下SoftAP的SSID前缀或全名（
-          XPGWifiSDK以此判断手机当前是否连上了SoftAP，AirLink配置时该参数无意义，
-          传nil即可）
+ SoftAPMode模式下SoftAP的SSID前缀或全名（
+ XPGWifiSDK以此判断手机当前是否连上了SoftAP，AirLink配置时该参数无意义，
+ 传nil即可）
  * @param wifiGAgentType types 配置的wifi模组类型列表，存放NSNumber对象，SDK默认同时发送庆科和汉枫模组配置包；
  *        SoftAPMode模式下该参数无意义。types为nil，SDK按照默认处理。如果只想配置庆科模组，types中请加入@XPGWifiGAgentTypeMXCHIP类；
  *        如果只想配置汉枫模组，types中请加入@XPGWifiGAgentTypeHF；如果希望多种模组配置包同时传，
@@ -52,13 +57,83 @@ exports.setDeviceWifiBindDevice = function (appid, productKey, wifiSSID, wifiKey
         wifiGAgentType
     ]);
 };
+/**
+ *  cordova 获取设备列表
+ *
+ *  @param command [appid,[productkey],uid,token]
+ */
 exports.getDeviceList = function (appid, productKey, uid, token, success, error) {
     exec(success, error, "gwsdk", "getDeviceList", [appid, productToArray(productKey), uid, token]);
 };
+/**
+ *  cordova 绑定设备
+ *
+ *  @param command ["appid","uid","token","did","passcode","remark"]
+ */
+exports.deviceBinding = function (appid, uid, token, did, passcode, remark) {
+    exec(success, error, "gwsdk", "deviceBinding", [appid, uid, token, did, passcode, remark])
+};
+/**
+ *  cordova 控制设备
+ *
+ *  @param command ["appid",["prodctkeys"],"uid","token","mac","value"]
+ */
 exports.deviceControl = function (appid, productKey, uid, token, mac, value, success, error) {
     exec(success, error, "gwsdk", "deviceControl", [appid, productToArray(productKey), uid, token, mac, value]);
 };
-
+/**
+ * cordova 获取ssid列表
+ *
+ *  @param command []
+ */
+exports.getWifiSSIDList = function (success, error) {
+    exec(success, error, "gwsdk", "getWifiSSIDList", []);
+};
+/**
+ *  cordova 开始device的监听
+ *
+ *  @param command []
+ */
+exports.startDevcieListener = function (success, error) {
+    exec(success, error, "gwsdk", "startDevcieListener", []);
+};
+/**
+ *  cordova 停止device的监听
+ *
+ *  @param command []
+ */
+exports.stopDeviceListener = function (success, error) {
+    exec(success, error, "gwsdk", "stopDeviceListener", []);
+};
+/**
+ * cordova 连接设备
+ *
+ *  @param command ["uid","token","did"]
+ */
+exports.connect = function (uid, token, did, success, error) {
+    exec(success, error, "gwsdk", "connect", [uid, token, did]);
+};
+/**
+ * cordova 断开连接
+ *
+ *  @param command ["did"]
+ */
+exports.disconnect = function (did, success, error) {
+    exec(success, error, "gwsdk", "disconnect", [did]);
+};
+/**
+ * cordova 发送控制命令
+ *
+ *  @param command ["did","value"]
+ */
+exports.write = function (did, value, success, error) {
+    exec(success, error, "gwsdk", "write", [did, value]);
+};
+/**
+ *  cordova 释放内存
+ *
+ *  @param command []
+ */
 exports.deAlloc = function (success, error) {
     exec(success, error, "gwsdk", "dealloc", []);
 };
